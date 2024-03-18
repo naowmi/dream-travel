d2718b05f42cacfa47f696eb23af1afb4fa81b82
naowmi <vadimpelyushenko@gmail.com>
2024-03-18 16:45:41 +0200

    soon

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import css from "./App.module.css";
+export default function App() {
+
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Coming soon</h1>
+
+    </div>
+  )
+}
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,8 @@
+import css from "./Background.module.css";
+export default function Background() {
+    return (
+        <div className={css.background}>
+
+        </div>
+    )
+}
